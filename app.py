e0d02b6b6bb526bb3c7030d5edad21806d1fcd1f
Sergei Soletskii <serey19992@yandex.ru>
2025-10-24 19:05:16 +0300

    Change url

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_INTERNAL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
