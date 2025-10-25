fcad0fe3f22fdb5450d33e2d67af644fea64a4ee
Sergei Soletskii <serey19992@yandex.ru>
2025-10-25 22:07:49 +0300

    aaassss

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from dotenv import load_dotenv
 import requests as rq
 import secrets
-
+import hashlib
+import base64
 from sqlalchemy.orm import state
 from werkzeug.exceptions import Forbidden
 
@@ -23,6 +24,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+
+def generate_pke_key():
+    code_verifier = secrets.token_urlsafe(64)
+    code_verifier_bytes = code_verifier.encode('utf-8')
+    sha256_hash = hashlib.sha256(code_verifier_bytes).digest()
+    code_challenge = base64.urlsafe_b64encode(sha256_hash).rstrip(b'=').decode('utf-8')
+    return code_verifier, code_challenge
+
 @app.route('/')
 def index():
     return render_template('first_page.html')
@@ -31,11 +40,11 @@ def index():
 @app.route('/tap_one', methods=['GET', 'POST'])
 def aut():
     state = secrets.token_urlsafe(16)
-    code_verifier = secrets.token_urlsafe(64)
+    code_verifier, code_challenge = generate_pke_key()
     session['state'] = state
     session['code_verifier'] = code_verifier
     # Передаём в шаблон (JS)
-    return render_template('tap_one.html', state=state)
+    return render_template('tap_one.html', state=state, code_challenge=code_challenge)
 
 
 @app.route('/callback', methods=['POST'])
