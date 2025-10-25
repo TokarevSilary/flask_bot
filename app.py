1049b62e8da2c4f1ed117744a3bccddebfdbf20e
Sergei Soletskii <serey19992@yandex.ru>
2025-10-25 21:10:59 +0300

    aaass

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests as rq
 import secrets
 
 from sqlalchemy.orm import state
+from werkzeug.exceptions import Forbidden
 
 from create_table.create_session import db
 from create_table.base_information import *
@@ -30,7 +31,9 @@ def index():
 @app.route('/tap_one', methods=['GET', 'POST'])
 def aut():
     state = secrets.token_urlsafe(16)
+    code_verifier = secrets.token_urlsafe(64)
     session['state'] = state
+    session['code_verifier'] = code_verifier
     # Передаём в шаблон (JS)
     return render_template('tap_one.html', state=state)
 
@@ -38,9 +41,30 @@ def aut():
 @app.route('/callback', methods=['POST'])
 def vk_callback():
     data = request.get_json(silent=True)
-    if session.get('state') == data['state']:
-        print(f"JSON: {data}")
+    if session.get('state') != data['state']:
+        return "Ошибка! Подменённый ответ", 400
+
+    code_verifier = session.get('code_verifier')
+    dat = {
+        "grant_type": 'authorization_code',
+        "code_verifier": code_verifier,
+        "redirect_uri": "https://flask-bot-lu45.onrender.com/callback?"
+                        "return_to=https://flask-bot-lu45.onrender.com/",
+        "code":data['code'],
+        "client_id": os.environ.get('CLIENT_ID'),
+        "device_id": data.get('device_id'),
+        "state": data.get('state')
+    }
+    response = rq.post("https://id.vk.ru/oauth2/auth",
+                       data=dat,
+                       headers={"Content-Type": "application/x-www-form-urlencoded"})
+    if response.ok:
+        token_data = response.json()
+        print(token_data)
+    else:
+        print(response.text)
     return "OK"
+
 
 
 if __name__ == '__main__':
